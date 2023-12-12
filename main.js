fec0e2bd93f5d431393ab58e0b195e3e8c29aea7
BallSpins <sandalupil@gmail.com>
2023-12-12 19:20:35 +0700

    rombak cik

--- main.js
+++ main.js
@@ -1,148 +1,89 @@
-const Dec = document.getElementById("Dec");
-const Bin = document.getElementById("Bin");
-const Hex = document.getElementById("Hex");
-const Oct = document.getElementById("Oct");
-const p1 = document.getElementById("p1");
-const p2 = document.getElementById("p2");
-const p3 = document.getElementById("p3");
-const p4 = document.getElementById("p4");
-const calc = document.getElementById("Calc");
-const putOn = document.getElementById("input");
-const Mbtn = document.querySelectorAll(".Mbtn button");
-const p1a = "";
-const p2a = "";
-const p3a = "";
-const p4a = "";
+let base = 10;
 
-function funcDec() {
-    var input = parseInt(document.getElementById("input").value);
-    var Bin = Number(input).toString(2);
-    var Hex = Number(input).toString(16);
-    var Oct = Number(input).toString(8);
-
-    p1.innerHTML = input
-    p2.innerHTML = Bin;
-    p3.innerHTML = Hex;
-    p4.innerHTML = Oct;
-
-}
-function funcBin() {
-    var input = parseInt(document.getElementById("input").value);
-    var Dec = parseInt(input, 2);
-    var Hex = Dec.toString(16);
-    var Oct = Dec.toString(8);
-
-    p1.innerHTML = Dec;
-    p2.innerHTML = input;
-    p3.innerHTML = Hex;
-    p4.innerHTML = Oct;
-}
-function funcHex() {
-    var input = document.getElementById("input").value;
-    var Dec = parseInt(input, 16);
-    var Bin = Dec.toString(2);
-    var Oct = Dec.toString(8);
-
-    p1.innerHTML = Dec;
-    p2.innerHTML = Bin;
-    p3.innerHTML = input;
-    p4.innerHTML = Oct;
-}
-function funcOct() {
-    var input = document.getElementById("input").value;
-    var Dec = parseInt(input, 8);
-    var Bin = Dec.toString(2);
-    var Hex = Dec.toString(16);
-
-    p1.innerHTML = Dec;
-    p2.innerHTML = Bin;
-    p3.innerHTML = Hex;
-    p4.innerHTML = input;
+window.onload = function() {
+    console.log("Loaded!");
 }
 
-function toDec() {
-    p1.innerHTML = p1a;
-    p2.innerHTML = p2a;
-    p3.innerHTML = p3a;
-    p4.innerHTML = p4a;
+const setColor = elem => {
+    for(let i = 0; i < elem.length; i++) {
+        elem[i][0].style.backgroundColor = elem[i][1];
+        elem[i][0].style.color = elem[i][2];
+    }
+}
+
+const toNaN = () => {
+    document.querySelectorAll("hasil").forEach(elem => {
+        elem.innerHTML = "";
+    });
     document.getElementById("input").value = "";
-    document.getElementById("Calc").setAttribute( "onClick", "javascript: funcDec();" );
-    //Dec
-    Dec.style.backgroundColor = 'blue';
-    Dec.style.color = 'white';
-    //Bin
-    Bin.style.backgroundColor = 'white';
-    Bin.style.color = 'black';
-    //Hex
-    Hex.style.backgroundColor = 'white';
-    Hex.style.color = 'black';
-    //Oct
-    Oct.style.backgroundColor = 'white';
-    Oct.style.color = 'black';
 }
-function toBin() {
-    p1.innerHTML = p1a;
-    p2.innerHTML = p2a;
-    p3.innerHTML = p3a;
-    p4.innerHTML = p4a;
-    document.getElementById("input").value = "";
-    document.getElementById("Calc").setAttribute( "onClick", "javascript: funcBin();" );
-    //Dec
-    Dec.style.backgroundColor = 'white';
-    Dec.style.color = 'black';
-    //Bin
-    Bin.style.backgroundColor = 'blue';
-    Bin.style.color = 'white';
-    //Hex
-    Hex.style.backgroundColor = 'white';
-    Hex.style.color = 'black';
-    //Oct
-    Oct.style.backgroundColor = 'white';
-    Oct.style.color = 'black';
+
+const Output = (elem, val) => {
+    console.log(elem);
+    console.log(val);
+    for(let i = 0; i < elem.length; i++) {
+        elem[i].innerHTML = val[i];
+    }
 }
-function toHex() {
-    p1.innerHTML = p1a;
-    p2.innerHTML = p2a;
-    p3.innerHTML = p3a;
-    p4.innerHTML = p4a;
-    document.getElementById("input").value = "";
-    document.getElementById("Calc").setAttribute( "onClick", "javascript: funcHex();" );
-    //Dec
-    Dec.style.backgroundColor = 'white';
-    Dec.style.color = 'black';
-    //Bin
-    Bin.style.backgroundColor = 'white';
-    Bin.style.color = 'black';
-    //Hex
-    Hex.style.backgroundColor = 'blue';
-    Hex.style.color = 'white';
-    //Oct
-    Oct.style.backgroundColor = 'white';
-    Oct.style.color = 'black';
+
+function Convert(input) {
+    console.log(base);
+    switch(base) {
+        case 2 : {
+            let Dec = parseInt(input, base);
+            let Hex = Dec.toString(16);
+            let Oct = Dec.toString(8);
+
+            Output([DecOut, BinOut, HexOut, OctOut], [Dec, input, Hex, Oct]);
+            break;
+        }
+
+        case 8 : {
+            let Dec = parseInt(input, base);
+            let Bin = Dec.toString(2);
+            let Hex = Dec.toString(16);
+
+            Output([DecOut, BinOut, HexOut, OctOut], [Dec, Bin, Hex, input]);
+            break;
+        }
+
+        case 10 : {
+            let Bin = Number(input).toString(2);
+            let Hex = Number(input).toString(16);
+            let Oct = Number(input).toString(8);
+
+            console.log(Hex);
+
+            Output([DecOut, BinOut, HexOut, OctOut], [input, Bin, Hex, Oct]);
+            break;
+        }
+
+        case 16 : {
+            let Dec = parseInt(input, base);
+            let Bin = Dec.toString(2);
+            let Oct = Dec.toString(8);
+
+            Output([DecOut, BinOut, HexOut, OctOut], [Dec, Bin, input, Oct]);
+            break;
+        }
+    }
 }
-function toOct() {
-    p1.innerHTML = p1a;
-    p2.innerHTML = p2a;
-    p3.innerHTML = p3a;
-    p4.innerHTML = p4a;
-    document.getElementById("input").value = "";
-    document.getElementById("Calc").setAttribute( "onClick", "javascript: funcOct();" );
-    //Dec
-    Dec.style.backgroundColor = 'white';
-    Dec.style.color = 'black';
-    //Bin
-    Bin.style.backgroundColor = 'white';
-    Bin.style.color = 'black';
-    //Hex
-    Hex.style.backgroundColor = 'white';
-    Hex.style.color = 'black';
-    //Oct
-    Oct.style.backgroundColor = 'blue';
-    Oct.style.color = 'white';
+
+function changeBase(button, iBase) {
+    base = iBase;
+    toNaN();
+    setColor([
+        [button, 'blue', 'white'], 
+        [Dec, iBase === 10 ? 'blue' : 'white',iBase === 10 ? 'white' : 'black'], 
+        [Hex, iBase === 16 ? 'blue' : 'white',iBase === 16 ? 'white' : 'black'], 
+        [Oct, iBase === 8 ? 'blue' : 'white', iBase === 8 ? 'white' : 'black'], 
+        [Bin, iBase === 2 ? 'blue' : 'white', iBase === 2 ? 'white' : 'black']
+    ]);
 }
-putOn.addEventListener("keypress", function(event) {
+
+document.getElementById("input").addEventListener("keypress", function(event) {
     if (event.key === 'Enter') {
     event.preventDefault();
-        calc.click();
+        document.getElementById("Calc").click();
     }
 });
